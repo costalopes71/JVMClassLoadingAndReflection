450ffa7949879264521a0fb5e112bb3298119bcf
Joao Lopes <joaolopes71@hotmail.com>
2019-08-18 23:54:41 -0300

    first and second module

---
+++ src/com/costalopes/Helper.java
@@ -0,0 +1,11 @@
+package com.costalopes;
+
+public class Helper {
+
+	public String getMessage() {
+		
+		return "Hello from Helper";
+
+	}
+
+}
---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		com.costalopes.Helper helper = new com.costalopes.Helper();
+		System.out.println(helper.getMessage());
+
+	}
+
+}
+
